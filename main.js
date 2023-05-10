06a0e3cb74d93285df9bdf7db65ba8ba0514e43a
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2023-05-09 23:00:38 -0700

    90% update
    All information and functions implemented. Text bug with the 5th slide in the carousel.

--- main.js
+++ main.js
@@ -26,3 +26,5 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 } 
+// This is not code that I wrote; I'm using it to controll my carousel.
+// Code barrowed from W3 tutorial.
